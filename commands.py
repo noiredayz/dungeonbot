582bd3921cd7aa66ef00d4df24bc9bb528531218
Noiredayz <noiredayz@protonmail.com>
2024-03-17 13:08:39 +0100

    added username fix for wr and xp commands

--- commands.py
+++ commands.py
@@ -245,7 +245,8 @@ def xp(user, display_name, channel, message=None):
             else:
                 util.send_message(messages.not_registered(display_name), channel)
     else:
-        target = db(opt.USERS).find_one({'username': re.compile('^' + re.escape(message) + '$', re.IGNORECASE)})
+		target_user = util.cutusername(message)
+        target = db(opt.USERS).find_one({'username': re.compile('^' + re.escape(target_user) + '$', re.IGNORECASE)})
         if target and target.get('user_level'):
             tags = db(opt.TAGS).find_one_by_id(target['_id'])
             if tags and tags.get('bot') == 1:
@@ -305,7 +306,8 @@ def winrate(user, display_name, channel, message=None):
             else:
                 util.send_message(messages.not_registered(display_name), channel)
     else:
-        target = db(opt.USERS).find_one({'username': re.compile('^' + re.escape(message) + '$', re.IGNORECASE)})
+		target_user = util.cutusername(message)
+        target = db(opt.USERS).find_one({'username': re.compile('^' + re.escape(target_user) + '$', re.IGNORECASE)})
         if target and target.get('user_level'):
             tags = db(opt.TAGS).find_one_by_id(target)
             if tags and tags.get('bot') == 1:
